bf583802f8fb69fcaec8e07942407cd0ff018c12
Eric Robinson <ericjrobinson@live.com>
2024-08-19 23:39:54 -0400

    Update pattern.cpp

--- Source/analyzer/pattern.cpp
+++ Source/analyzer/pattern.cpp
@@ -403,7 +403,7 @@ bool MatchesSolidPattern()
 		}
 	}
 
-	std::cout << "PATTERN: Potential dungeon level " << (int)currlevel << " seed: " << sgGameInitInfo.dwSeed << std::endl;
+	std::cout << sgGameInitInfo.dwSeed  << " :: PATTERN: Potential dungeon level " << (int)currlevel << " seed located." << std::endl;
 
 	return true;
 }
@@ -454,7 +454,7 @@ static bool MatchesObjectPattern()
 			return false;
 	}
 
-	std::cout << "PATTERN: Potential dungeon level " << (int)currlevel << " seed: " << sgGameInitInfo.dwSeed << std::endl;
+	std::cout << sgGameInitInfo.dwSeed << " :: PATTERN: Potential dungeon level " << (int)currlevel << " seed located." << std::endl;
 
 	return true;
 }
